e172fa377e2458e8470c5faf20e094fd728db239
Tiz <dannilo@bk.ru>
2024-12-20 12:47:20 +0400

    Start

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace _15_6_6_Praktika
-{
-    internal class Program
+{    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // ???
+
+            return null;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
         }
     }
 }
